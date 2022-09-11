e9d135393fa7307ecaf22066121f47f42181c7aa
aman-janwani <amanjanwani1486@gmail.com>
2022-09-11 15:37:44 +0530

    Test2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
 
-import 'react-toastify/dist/ReactToastify.css';
-import { AudioPlayerProvider } from '../src/provider/audio-player';
+import "react-toastify/dist/ReactToastify.css";
+import { AudioPlayerProvider } from "../src/provider/audio-player";
 // import PageLayout from '../src/layouts/page-layout';
-import { ThemeProvider } from 'next-themes';
+import { ThemeProvider } from "next-themes";
 // import { AppWeb3Provider } from '../src/provider/app-web3';
-// import { ThirdwebProvider } from "@3rdweb/react";
-import { NextPage } from 'next';
-import { ReactElement, ReactNode } from 'react';
-import { ToastContainer } from 'react-toastify';
-import { RinkeByChainID } from '../src/constants';
-import '../styles/globals.css';
+import { ThirdwebProvider as ThirdProvider } from "@3rdweb/react";
+import { NextPage } from "next";
+import { ReactElement, ReactNode } from "react";
+import { ToastContainer } from "react-toastify";
+import { RinkeByChainID } from "../src/constants";
+import "../styles/globals.css";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 type NextPageWithLayout = NextPage & {
@@ -25,17 +25,20 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
 
   return (
-    <ThirdwebProvider  
-      desiredChainId={ChainId.Rinkeby}
-    >
-      {/* <AppWeb3Provider> */}
+    <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
+      <ThirdProvider
+        supportedChainIds={RinkeByChainID}
+        connectors={{
+          injected: {},
+        }}
+      >
         <AudioPlayerProvider>
-           <ThemeProvider attribute="class" defaultTheme="light">
+          <ThemeProvider attribute="class" defaultTheme="light">
             {getLayout(<Component {...pageProps} />)}
             <ToastContainer />
           </ThemeProvider>
         </AudioPlayerProvider>
-      {/* </AppWeb3Provider> */}
+      </ThirdProvider>
     </ThirdwebProvider>
   );
 }
